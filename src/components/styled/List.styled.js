5c7927295ffc69eb1b9d9237c667cfd816da3c17
jawad Bin Azam <abidibnazam@gmail.com>
2023-11-03 01:33:14 +0600

    minor fixing

--- src/components/styled/List.styled.js
+++ src/components/styled/List.styled.js
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 export const List = styled.li`
   border: 1px solid #444947;
-  border-radius: 2px;
-  padding: 3px 0px;
+  border-radius: 50%;
+  padding: 4px 4px;
   color: #444947;
   text-decoration: none;
+  font-size: 14px;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 10px;
+  }
 `;
